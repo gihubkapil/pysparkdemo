c513a9696fa5fce040333c1a8e93beab9a76dd7c
kapil <kapilabrol93@gmail.com>
2022-08-09 21:39:48 +0530

    update setup file

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import find_packages, setup
-data_files = [('config/', ['app/config/conf.yaml'])]
+data_files = [('config', ['app/config/conf.yaml'])]
 
 setup(
     name='pysparkdemo',
     extras_require=dict(tests=['pytest']),
-    packages=find_packages(where="app"),
-    package_dir={"": "app"},
+    packages=['app', 'app.config', 'app.utils'],
+    #packages=find_packages('app'),
+    #package_dir={"": "app"},
     data_files=data_files
 )
